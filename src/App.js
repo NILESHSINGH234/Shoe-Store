675615ce9ea8f4f30d6b89c51782be759280f577
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-03-16 19:43:39 +0530

    Done With All Setup Files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.png";
+import { Home } from "./pages/Home/Home";
+import { Products } from "./pages/Products/Products";
+import { Wishlist } from "./pages/Wishlist/Wishlist";
+import { Cart } from "./pages/Cart/Cart";
+import { Login } from "./pages/Login/Login";
+import Mockman from "mockman-js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
+        <Route path="/wishlist" element={<Wishlist></Wishlist>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/mock" element={<Mockman></Mockman>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import Mock from 'mockman-js';
+import{ BrowserRouter} from 'react-router-dom'
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    
+      <App />
+      
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
